0ceea19aaa747cb3cb6dfc90353c78def3b98b6d
Reehan <syedreehan998@gmail.com>
2023-10-15 23:47:57 +0530

    Included Clustering

---
+++ Part A/3.R
@@ -0,0 +1,19 @@
+# Load necessary libraries
+library(cluster)
+library(ggplot2)
+
+# Use the iris dataset and exclude the Species column for clustering
+x <- iris[-5]
+
+model <- kmeans(x,centers =  3)
+
+# Add cluster assignments to the data
+iris$cluster <- as.factor(model$cluster)
+
+# Use clusplot for visualization
+clusplot(x, model$cluster, main="2D representation of the Cluster solution")
+
+# Optionally, plot Sepal.Length vs Sepal.Width with ggplot for comparison
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = cluster)) + 
+  geom_point() + 
+  labs(title = "K-means Clustering of Iris Dataset with ggplot2")
